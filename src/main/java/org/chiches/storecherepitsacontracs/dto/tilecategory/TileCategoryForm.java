2ef34dae9e0133d0d2f32bf24a6b9e3219111f04
1.kla$ <recum12233@mail.ru>
2024-12-08 20:33:01 +0300

    update

--- src/main/java/org/chiches/storecherepitsacontracs/dto/tilecategory/TileCategoryForm.java
+++ src/main/java/org/chiches/storecherepitsacontracs/dto/tilecategory/TileCategoryForm.java
@@ -2,7 +2,16 @@ package org.chiches.storecherepitsacontracs.dto.tilecategory;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record TileCategoryForm(
-        @NotBlank(message = "Name cannot be blank")
-        String name
-) { }
+public class TileCategoryForm {
+    @NotBlank
+    private String name;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
